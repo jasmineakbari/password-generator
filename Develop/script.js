f607e147391782ce1764ab2346531d57fe04aaea
unknown <jasmineakbari@yahoo.com>
2020-11-01 13:06:27 -0500

    created validation feature to password length

--- Develop/script.js
+++ Develop/script.js
@@ -12,6 +12,10 @@ var generateButton = function() {
   // Prompts to record pw parameters starting with Number of Characters in Password
   var passwordLength = window.prompt("Choose how many characters long you would like your password, it can be between 8 and 128 total characters long.");
   console.log(passwordLength);
+  if (passwordLength <= 7 || passwordLength >= 129) {
+    window.alert("That is not a valid password length, please try again.");
+    window.prompt("Choose how many characters long you would like your password, it can be between 8 and 128 total characters long.");;
+  }
 
   // LowerCase
   var lowerCase = confirm("Do you want Lowercase letters in your password?");
@@ -29,12 +33,15 @@ var generateButton = function() {
   var special = confirm("Do you want Special Characters in your password?");
   console.log(special);
 
+  
+
+
+
   //Rerun Password parameters if atleast one parameter is not selected
   if(lowerCase === false && upperCase === false && number === false && special === false) {
     window.alert("You must select at least One Password Parameter: \n1. Lowercase\n2. Uppercase\n3. Number\n4. Special Character")
     generateButton();
   }
-
 
 
   // Get references to the #generate element
