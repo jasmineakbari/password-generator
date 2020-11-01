5884047144ad5574a81d0034c0ce51f4d0cc7ef7
unknown <jasmineakbari@yahoo.com>
2020-11-01 12:20:15 -0500

    required atleast one parameter before password can be generated

--- Develop/script.js
+++ Develop/script.js
@@ -10,7 +10,7 @@ function writePassword() {
 var generateButton = function() {
   window.alert("Please set your password parameters.");
   // Prompts to record pw parameters starting with Number of Characters in Password
-  var passwordLength = window.prompt("Choose how many characters long you would like your password, it can be between 8 and 128.");
+  var passwordLength = window.prompt("Choose how many characters long you would like your password, it can be between 8 and 128 total characters long.");
   console.log(passwordLength);
 
   // LowerCase
@@ -25,11 +25,15 @@ var generateButton = function() {
   var number = confirm("Do you want Numbers in your password?");
   console.log(number);
 
-  // Special Characters " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  // Special Characters " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~" pick 10
   var special = confirm("Do you want Special Characters in your password?");
   console.log(special);
 
-
+  //Rerun Password parameters if atleast one parameter is not selected
+  if(lowerCase === false && upperCase === false && number === false && special === false) {
+    window.alert("You must select at least One Password Parameter: \n1. Lowercase\n2. Uppercase\n3. Number\n4. Special Character")
+    generateButton();
+  }
 
 
 
